b911218461718a45401286682c0918d96b1dd706
Cindy <happycindy26@gmail.com>
2019-11-03 14:25:38 +1100

    score works now

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let button = document.getElementById('coin');
+let scoreCount = document.getElementById('score');
+let count = 0;
 
 button.onclick = () => {
     button.style.left = (Math.random() * 500) + 'px';
-    button.style.top = Math.random() * 500 + 'px'
-    console.log('hello Surgey!')
+    button.style.top = Math.random() * 500 + 'px';
+    count += 1;
+    scoreCount.innerHTML = `${count}`;
 }

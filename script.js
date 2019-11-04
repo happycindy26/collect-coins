f1cb090f42a4146c98118f22fe7585d15a0f0fcd
Cindy <happycindy26@gmail.com>
2019-11-04 16:08:08 +1100

    reset timeout onclick

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let coin = document.getElementById('coin');
 let scoreCount = document.getElementById('score');
 let count = 0;
+let time = null;
 
 const buttonClickHandler = () => {
     coin.style.left = Math.random() * 500 + 'px';
     coin.style.top = Math.random() * 500 + 'px';
     count += 1;
     scoreCount.innerHTML = `${count}`;
+    clearTimeout(time);
+    moveCoinIn2Sec();
+
 }
 coin.onclick = buttonClickHandler;
 /*
@@ -18,13 +22,15 @@ button.onclick = () => {
 }
 */
 const moveCoinOnTime = () => {
+    console.log('moved');
     coin.style.left = Math.random() * 500 + 'px';
     coin.style.top = Math.random() * 500 + 'px';
     moveCoinIn2Sec();
 }
 
 const moveCoinIn2Sec = () => {
-    setTimeout(moveCoinOnTime, 2000);
+    time = setTimeout(moveCoinOnTime, 2000);
 }
 
 moveCoinIn2Sec();
+
